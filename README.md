a4ac6ce08646e7f3299365e41fabcfdd090ddc09
Chris Butler <chris@thebutlers.me>
2021-07-02 16:57:03 +1000

    feat: ISM script.
    Signed-off-by: Chris Butler <chris@thebutlers.me>

--- README.md
+++ README.md
@@ -1,77 +1,38 @@
-<!-- This should be the location of the title of the repository, normally the short name -->
-# repo-template
+# compliance-trestle-demos
 
-<!-- Build Status, is a great thing to have at the top of your repository, it shows that you take your CI/CD as first class citizens -->
-<!-- [![Build Status](https://travis-ci.org/jjasghar/ibm-cloud-cli.svg?branch=master)](https://travis-ci.org/jjasghar/ibm-cloud-cli) -->
+A project capturing a number of demos for the [compliance-trestle](https://ibm.github.io/compliance-trestle) project including various additional scripts and transformers.
 
-<!-- Not always needed, but a scope helps the user understand in a short sentance like below, why this repo exists -->
-## Scope
+## Using / management of this repository
 
-The purpose of this project is to provide a template for new open source repositories.
+This project follows the same methodologies as within the main trestle project in terms of [contributing and developer setup](https://ibm.github.io/compliance-trestle/contributing/mkdocs_contributing/). Please submit [issues here](https://github.com/IBM/compliance-trestle/issues/new/choose) relating to this project.
 
-<!-- A more detailed Usage or detailed explaination of the repository here -->
-## Usage
+# Demonstrations
 
-This repository contains some example best practices for open source repositories:
+## Australian government Information Security Manual (ISM)
 
-* [LICENSE](LICENSE)
-* [README.md](README.md)
-* [CONTRIBUTING.md](CONTRIBUTING.md)
-* [MAINTAINERS.md](MAINTAINERS.md)
-<!-- A Changelog allows you to track major changes and things that happen, https://github.com/github-changelog-generator/github-changelog-generator can help automate the process -->
-* [CHANGELOG.md](CHANGELOG.md)
+This demonstration uses trestle as an SDK for interacting generating OSCAL files.
+A script `scripts/ISM/ISM.py` generates a set of profiles (for each security level) and a catalog per ISM version.
+Read more about the demo [here](./scripts/ISM/ISM.py).
 
-> These are optional
-
-<!-- The following are OPTIONAL, but strongly suggested to have in your repository. -->
-* [dco.yml](.github/dco.yml) - This enables DCO bot for you, please take a look https://github.com/probot/dco for more details.
-* [travis.yml](.travis.yml) - This is a example `.travis.yml`, please take a look https://docs.travis-ci.com/user/tutorial/ for more details.
-
-These may be copied into a new or existing project to make it easier for developers not on a project team to collaborate.
-
-<!-- A notes section is useful for anything that isn't covered in the Usage or Scope. Like what we have below. -->
-## Notes
-
-**NOTE: While this boilerplate project uses the Apache 2.0 license, when
-establishing a new repo using this template, please use the
-license that was approved for your project.**
-
-**NOTE: This repository has been configured with the [DCO bot](https://github.com/probot/dco).
-When you set up a new repository that uses the Apache license, you should
-use the DCO to manage contributions. The DCO bot will help enforce that.
-Please contact one of the IBM GH Org stewards.**
-
-<!-- Questions can be useful but optional, this gives you a place to say, "This is how to contact this project maintainers or create PRs -->
-If you have any questions or issues you can create a new [issue here][issues].
-
-Pull requests are very welcome! Make sure your patches are well tested.
-Ideally create a topic branch for every separate change you make. For
-example:
-
-1. Fork the repo
-2. Create your feature branch (`git checkout -b my-new-feature`)
-3. Commit your changes (`git commit -am 'Added some feature'`)
-4. Push to the branch (`git push origin my-new-feature`)
-5. Create new Pull Request
-
-## License
-
-All source files must include a Copyright and License header. The SPDX license header is 
-preferred because it can be easily scanned.
+## License & Authors
 
 If you would like to see the detailed LICENSE click [here](LICENSE).
+Consult [contributors](https://github.com/IBM/compliance-trestle/graphs/contributors) for a list of authors and [maintainers](MAINTAINERS.md) for the core team.
+
+Note that some content referenced within this repository is under separate licenses and is annotated as such.
 
 ```text
+# Copyight (c) 2021 IBM Corp. All rights reserved.
 #
-# Copyright 2020- IBM Inc. All rights reserved
-# SPDX-License-Identifier: Apache2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 ```
-## Authors
-
-Optionally, you may include a list of authors, though this is redundant with the built-in
-GitHub list of contributors.
-
-- Author: New OpenSource IBMer <new-opensource-ibmer@ibm.com>
-
-[issues]: https://github.com/IBM/repo-template/issues/new
---
+++ scripts/ISM/ISM.py
@@ -0,0 +1,263 @@
+#!/usr/bin/env python3
+# # -*- mode:python; coding:utf-8 -*-
+# Copyright (c) 2020 IBM Corp. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# limitations under the License.
+"""Create ISM catalogs.
+
+This script is used to convert Australian government ISM into OSCAL formats.
+It does this via pulling the ISM xml doc and creating:
+
+1 Catalog for all the controls
+4 profiles (Official, protected, secret, TS)
+
+Ideally this would be a cron job based script, however, as ACSC publish revisions
+with specific names this would need to be discovered by crawling. This will be a potential future enhancement.
+
+This script pulls down teh controls in a 'dumb' way from the xml to get the actual controls. A full featured catalog
+will need to parse appropriate word / xml documents to provide groups /guidance.
+"""
+import io
+import json
+import logging
+import pathlib
+import sys
+import urllib.request
+import zipfile
+from datetime import datetime
+from uuid import uuid4
+
+from ilcli import Command
+
+import trestle.oscal.catalog as catalog
+import trestle.oscal.common as common
+import trestle.oscal.profile as profile
+
+import xmltodict
+
+# Globally define logging behaviour.
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+remarks_tuple = '\n'.join(
+    [
+        'This is not an official version of the Australian Government Information Security Manual.',
+        '',
+        'Find the official versions here: https://www.cyber.gov.au/acsc/view-all-content/ism',
+        'This content was generated using scrips/ISM/ISM.py'
+    ]
+)
+
+
+class ISMManager():
+    """ISMManager a class to manage conversion of ISM artifacts into OSCAL."""
+
+    def __init__(self):
+        """Initialize ISM manager. No required parameters."""
+        self._profile_controls = {'OFFICIAL': [], 'PROTECTED': [], 'SECRET': [], 'TOP_SECRET': []}
+        self._profiles = {}
+
+    def fetch_ism(self, url):
+        """Fetch an Australian government ISM and covert to a dict."""
+        logger.debug('Fetching ISM from: ' + url)
+        request_url = urllib.request.urlopen(url)
+        document = request_url.read()
+        zipfile_content = zipfile.ZipFile(io.BytesIO(document))
+        content_list = zipfile_content.namelist()
+        xml_files = [x for x in content_list if '.xml' in x]
+        assert len(xml_files) == 1
+        self.ism_xml = xmltodict.parse(zipfile_content.open(xml_files[0]).read())
+
+    def _populate_control_list(self, control, raw_id):
+        """Populate control lists based on a dict from the xml version of the ISM."""
+        # TODO: Really not pythonic but anyway.
+        control_id = 'control-' + raw_id
+
+        for security_level in self._profile_controls.keys():
+            # Dealing with schema changes 'Yes' and 'true' appear to both be valid options.
+            if control[security_level].lower() == 'yes' or control[security_level].lower() == 'true':
+                self._profile_controls[security_level].append(control_id)
+
+    def _probe_for_keys(self, ism_control):
+        """Probe for the appropriate keys for l2 groups based on whether or not section exists."""
+        l2_group_key = 'Section'
+
+        if l2_group_key not in ism_control.keys():
+            l2_group_key = 'Topic'
+        return l2_group_key
+
+    def _name_clean(self, name: str) -> str:
+        """Normalize string to ncname format."""
+        return input.strip().lower().replace(' ', '_').replace('/', '-')
+
+    def create_ism_catalogs(self, version: str) -> None:
+        """Parse ISM object and create a catalog."""
+        m = common.Metadata(
+            **{
+                'title': 'Australian Government Information Security manual',
+                'last-modified': datetime.now().astimezone(),
+                'version': version,
+                'oscal-version': '1.0.0',
+                'remarks': remarks_tuple
+            }
+        )
+        ism_catalog = catalog.Catalog(metadata=m, uuid=str(uuid4()))
+
+        # Create basic metadata:
+        ism_controls = self.ism_xml['ISM']['Control']
+        l2_group_key = self._probe_for_keys(ism_controls[0])
+        """
+        Approach:
+            - Two levels of groups - no sub controls.
+            - below this will be parts
+        """
+        # Get list of top level controls
+        tl_group_titles = set(map(lambda x: x['Guideline'], ism_controls))
+        groups = []
+        for tl_group_name in tl_group_titles:
+            group = catalog.Group(id=self._name_clean(tl_group_name), title=tl_group_name)
+            # now add l2 groups
+            control_subset = list(filter(lambda x: x['Guideline'] == tl_group_name, ism_controls))
+            # get set l2 group names.
+            l2_group_titles = set(map(lambda x: x[l2_group_key], control_subset))
+            l2_groups = []
+            for l2_group_name in l2_group_titles:
+                clean_id = self._name_clean(l2_group_name)
+                l2_group = catalog.Group(id=clean_id, title=l2_group_name)
+                # Now identify and add the controls
+                oscal_controls = []
+                l2_control_subset = list(filter(lambda x: x[l2_group_key] == l2_group_name, control_subset))
+                # now we can create and add controls.
+                # TODO: Make more pythonic
+                for ism_control in l2_control_subset:
+                    raw_id = ism_control['Identifier']
+
+                    description = ism_control['Description']
+                    topic = ism_control['Topic']
+                    # make description the part statement
+                    statement_part = common.Part(id='control-' + raw_id + '-stmt', name='statement', prose=description)
+                    # this is very minimial
+                    oscal_control = catalog.Control(
+                        id='control-' + raw_id, title=topic + ' ' + raw_id, parts=[statement_part]
+                    )
+                    self._populate_control_list(ism_control, raw_id)
+                    oscal_controls.append(oscal_control)
+                l2_group.controls = oscal_controls
+                l2_groups.append(l2_group)
+            group.groups = l2_groups
+            groups.append(group)
+        ism_catalog.groups = groups
+        self._ism_catalog = ism_catalog
+
+    def create_ism_profiles(self, revision_date, uri='./ISM_catalog.yaml'):
+        """Create profile for each ISM environment."""
+        for security_level in self._profile_controls.keys():
+            ism_profile = profile.Profile(
+                uuid=str(uuid4()),
+                metadata=common.Metadata(
+                    **{
+                        'title': 'Australian Government Information Security Manual profile for ' + security_level,
+                        'version': revision_date,
+                        'oscal-version': '1.0.0',
+                        'last-modified': datetime.now().astimezone(),
+                        'remarks': remarks_tuple
+                    }
+                ),
+                imports=[profile.Import(href=uri)]
+            )
+            controls_list = self._profile_controls[security_level]
+            ism_profile.imports[0].include_controls = self._populate_import_include(controls_list)
+            self._profiles[security_level] = ism_profile
+
+    def _populate_import_include(self, control_list):
+        include_controls = []
+        selector = profile.SelectControlById()
+        selector.with_ids = control_list
+        include_controls.append(selector)
+        return include_controls
+
+    def write_catalog(self, catalogs_path, ism_name):
+        """Wrap and write oscal catalog object."""
+        ism_dir_path = catalogs_path / ism_name
+        ism_dir_path.mkdir(exist_ok=True)
+        ism_file_path = ism_dir_path / 'catalog.json'
+        self._ism_catalog.oscal_write(ism_file_path)
+
+    def write_profiles(self, profiles_dir, ism_name):
+        """Write out all profiles."""
+        for security_level in self._profiles.keys():
+            profile_dir = profiles_dir / (ism_name + '_' + security_level)
+            profile_dir.mkdir(exist_ok=True)
+            profile_path = profile_dir / 'profile.json'
+            self._profiles[security_level].oscal_write(profile_path)
+
+
+class ISM(Command):
+    """
+    Convert the Australian goverment information security manual (in various versions) into catalogs and profiles.
+
+    This CLI has presumptions on resource structures that are returned.
+
+    Please note that this project current presumes information about the project structure.
+    """
+
+    def _init_arguments(self):
+        self.add_argument('-r', '--root-dir', help='Trestle project root.', default='./')
+
+    def _run(self, args):
+        # little test
+        root_dir = pathlib.Path(args.root_dir).resolve()
+        catalogs_dir = root_dir.joinpath('catalogs').resolve()
+        profiles_dir = root_dir.joinpath('profiles').resolve()
+        cache_dir = root_dir.joinpath('non-oscal-data').resolve()
+        ism_json_file = root_dir.joinpath('scripts/ISM/ism_editions.json').resolve()
+
+        if not root_dir.exists():
+            logger.error('Root trestle project does not exist')
+            return 1
+        if not catalogs_dir.exists():
+            logger.error('Catalogs directory does not exist.')
+            return 1
+
+        if not profiles_dir.exists():
+            logger.error('Profiles directory does not exist.')
+            return 1
+
+        if not cache_dir.exists():
+            logger.error('Cache directory does not exist.')
+            return 1
+
+        ism_versions = json.load(ism_json_file.open())
+
+        for ism_file in ism_versions['isms']:
+            # ISM file format: 'ISM - List of Security Controls (August 2019).xml'
+            logger.info(ism_file)
+            url = ism_file['version_url']
+            ism_manager = ISMManager()
+            ism_manager.fetch_ism(url)
+            revision_date = ism_file['version_name'].split()
+            revision_string = revision_date[0] + '_' + revision_date[1]
+            logger.info(f'Revision date: {revision_date}')
+            logger.info(f'Revision string: {revision_string}')
+            logger.info(revision_string)
+            ism_name = 'ISM_' + revision_string
+
+            ism_manager.create_ism_catalogs(revision_string)
+            # This is presumed to be relative for now to the catalog repo based on this
+            ism_manager.write_catalog(catalogs_dir, ism_name)
+            ism_manager.create_ism_profiles(revision_string, '../../catalogs/' + ism_name + '/catalog.json')
+            ism_manager.write_profiles(profiles_dir, ism_name)
+
+
+if __name__ == '__main__':
+    sys.exit(ISM().run())
